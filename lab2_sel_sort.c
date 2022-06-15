5dea3e5604495408ed12c4e146455bc62a3db30f
vinaykulk32 <84218725+vinaykulk32@users.noreply.github.com>
2022-06-15 10:13:46 +0530

    Update lab2_sel_sort.c

--- lab2_sel_sort.c
+++ lab2_sel_sort.c
@@ -1,44 +1,38 @@
 #include<stdio.h>
 #include<time.h>
-
-
+void sort_it(int n,int a[]);
 int main()
 {
  int nArr[10000];
- int nMin=0;
+ int lowest=0;
  int t=0, i=0, j=0;
  clock_t end,start;
 
 int n=1000;
  //sort array using selection sort
- while(n<=5000){
-        int a[i];
-        start=clock();
-for(i=0;i<n;i++)
-{
-nArr[i]=rand();
-}
-    for(i=0; i<10000-1 ;i++)
- {
- nMin=i;
- for(j=i+1; j<10000;j++)
- {
- if(nArr[j]<nArr[nMin])
- nMin = j;
- }
-
- t=nArr[i];
- nArr[i]=nArr[nMin];
- nArr[nMin]=t;
- }
- int temp;
- for(j=0;j<50000000;j++)
-{
-temp=38/600;
-}
+ while(n<=10000){
+int a[i];
+start=clock();
+sort_it(n,nArr);
+ for(j=0;j<50000000;j++){int temp=38/600;}
 end=clock();
 printf("Time for n=%d is %f seconds \n",n,(((double)(end-start))/CLOCKS_PER_SEC));
  n+=1000;
  }
  return 0;
+}
+
+void sort_it(int n,int a[]){
+    int min,temp[n],var,i,j;
+    for(i=0;i<n-1;i++){
+        min=i;
+        for(j=i+1;j<n;j++){
+            if(a[j]<a[min]){
+                min=j;
+            }
+        }
+        var=a[i];
+        a[i]=a[min];
+        a[min]=var;
+    }
 }
