78d8a5101c8219b25872d9f244bdf7adab945acc
vinaykulk32 <84218725+vinaykulk32@users.noreply.github.com>
2022-07-20 10:15:58 +0530

    Update insertion-sort.c

--- insertion-sort.c
+++ insertion-sort.c
@@ -9,6 +9,7 @@ void main()
     int a[15000],n,i,j,ch,temp;
     clock_t start,end;
     n = 500;
+    n = 500;
     while(n <= 14500){
         for(i=0;i<n;i++){a[i] = rand();}
         start = clock();
@@ -17,39 +18,6 @@ void main()
         end = clock();
         printf("\n Time taken to sort %d elements is %f seconds. \n",n , (((double)(end - start))/CLOCKS_PER_SEC));
         n = n + 1000;
-    }
-        printf("\n 1. For manual entry of N value and array elements ");
-        printf("\n 2. To display time taken for sorting number of elements N in the range 500 to 14500 ");
-        printf("\n 3. To exit ");
-        printf("\n Enter your choice : ");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-        case 1 :  printf("\n Enter the number of elements : ");
-                  scanf("%d",&n);
-                  printf("\n Enter the array elements : ");
-                  for(i=0;i<n;i++)
-                        scanf("%d",&a[i]);
-
-                  start = clock();
-                  insertionSort(n,a);
-                   for(j=0;j<50000000;j++)
-                      temp = 38/600;
-                  end=clock();
-
-                    printf("\n Sorted array is : ");
-                  for(i=0;i<n;i++)
-                        printf("%d \t",a[i]);
-
-                  printf("\n Time taken to sort %d elements is %1.10f seconds. \n",n , (((double)(end - start))/CLOCKS_PER_SEC));
-                  break;
-
-          case 2 :
-                  break;
-
-        case 3 : exit(0);
-        }
-        getchar();
     }
 }
 void insertionSort(int n, int a[])
